f1b0a485e4e45b4beaf20c655623223a2c6537e1
william <william@DESKTOP-OL0HJ5R>
2026-01-17 16:31:30 -0300

    Device.cs - Added Class Device DeviceState.cs - Added Enum related to Device State

---
+++ Training/Training/Domain/Entities/Device.cs
@@ -0,0 +1,12 @@
+﻿using TrainingApi.Domain.Enums;
+
+namespace TrainingApi.Domain.Entities;
+
+public class Device
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public DeviceState State { get; set; }
+    public DateTime CreationTime { get; set; }
+}
---
+++ Training/Training/Domain/Enums/DeviceState.cs
@@ -0,0 +1,8 @@
+﻿namespace TrainingApi.Domain.Enums;
+
+public enum DeviceState
+{
+    Available,
+    InUse,
+    Inactive
+}
