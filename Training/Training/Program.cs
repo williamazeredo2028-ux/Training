b1746de7282696bd404e3babfdc6a66e2f3c1763
william <william@DESKTOP-OL0HJ5R>
2026-01-17 16:04:54 -0300

    TrainingApi.csproj - Added Swagger, EntityFramework and JsonPatch Packages Program.cs - Added configuration of services and middleware of Swagger

--- Training/Training/Program.cs
+++ Training/Training/Program.cs
@@ -1,3 +1,6 @@
+using Training;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,11 +9,19 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Training API", Version = "v1" });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Training API v1"));
     app.MapOpenApi();
 }
 
